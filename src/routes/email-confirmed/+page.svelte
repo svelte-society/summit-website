e254f2a42b4b47e70e8699141c6746712f8f12c9
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-03-08 21:53:35 +0100

    add email confirmed page

---
+++ src/routes/email-confirmed/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<div class="cover">
+	<div class="cover-center grid place-items-center gap-6">
+		<h1 class="font-display text-5xl text-center">Thank you! Your email is now confirmed.</h1>
+	</div>
+	<div class="mx-auto">
+		<div class="grid place-items-center md:flex-row w-full gap-3 px-">
+			<p>Go back to the landing page 👇</p>
+			<Button wide secondary href="/">Go back!</Button>
+		</div>
+	</div>
+</div>
