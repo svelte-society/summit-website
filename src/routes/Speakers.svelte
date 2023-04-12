e7619b7f1c7aa42d28d96eadbb6b73a1fa173bee
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-04-12 21:21:48 +0200

    set up rudimentary sessions block

--- src/routes/Speakers.svelte
+++
@@ -1,6 +0,0 @@
-<article class="bg-crystal-600 text-black">
-	<div class="grid gap-4 w-full sm:w-4/5 md:w-3/5 mx-auto px-4 py-24 text-center">
-		<h2 class="font-semibold text-2xl">Speakers</h2>
-		<hr class="border-black border-2 rounded-lg w-52 mx-auto" />
-	</div>
-</article>
