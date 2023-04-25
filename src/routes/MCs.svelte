0416ff213c0fef1280e63e304b47a0ebe6ea5fc6
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-04-25 10:56:09 +0200

    fix alignment issue on speakers and mcs

--- src/routes/MCs.svelte
+++ src/routes/MCs.svelte
@@ -9,7 +9,7 @@
 	<div class="grid gap-4 w-full sm:w-4/5 md:w-3/5 mx-auto px-4 pt-24 text-center">
 		<h2 class="font-semibold text-2xl">Masters of Ceremony</h2>
 		<hr class="border-black border-2 rounded-lg w-52 mx-auto" />
-		<ul class="grid gap-4 max-w-4xl lg:grid-cols-2 text-left">
+		<ul class="grid gap-4 max-w-4xl lg:grid-cols-2 text-left mx-auto">
 			{#each mcs as { name, tagline, id, profile }}
 				<li class="flex flex-col place-items-center gap-2 bg-violet-500 p-4 rounded-lg">
 					{#if profile}
--- src/routes/Sessions.svelte
+++ src/routes/Sessions.svelte
@@ -8,11 +8,11 @@
 	export let sessions: TalksResponse<SpeakerExpand>[];
 </script>
 
-<article id="speakers" class="bg-crystal-600 text-blac">
+<article id="speakers" class="bg-crystal-600 text-black">
 	<div class="grid gap-4 w-full sm:w-4/5 md:w-3/5 mx-auto px-4 py-24 text-center">
 		<h2 class="font-semibold text-2xl">Speakers</h2>
 		<hr class="border-black border-2 rounded-lg w-52 mx-auto" />
-		<ul class="grid gap-4 max-w-4xl lg:grid-cols-2 text-left">
+		<ul class="grid gap-4 max-w-4xl lg:grid-cols-2 text-left mx-auto">
 			{#each sessions as session}
 				<Session {session} />
 			{/each}
