d84f6f9dca9de4ba4ac64e9eb3cec99a39b70eab
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-05-05 18:27:23 +0200

    update text

--- src/routes/Sponsors.svelte
+++ src/routes/Sponsors.svelte
@@ -8,7 +8,7 @@
 
 <article id="sponsors" class="bg-papaya-400 text-black">
 	<div class="grid gap-4 w-full sm:w-4/5 md:w-3/5 mx-auto px-4 md:py-16 py-8 text-center max-w-4xl">
-		<h2 class="font-semibold text-2xl">Sponsors</h2>
+		<h2 class="font-semibold text-2xl">Sponsor</h2>
 		<hr class="border-black border-2 rounded-lg w-52 mx-auto" />
 		<div class="grid gap-3">
 			<ul class="grid grid-cols-1 gap-3">
