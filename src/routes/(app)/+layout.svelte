fdd83ab98831bc5db1389541f10381604895a923
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-05-08 21:30:33 +0200

    move files into layout groups

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { Head } from 'svead';
+	import Link from './NavLink.svelte';
+	import { page } from '$app/stores';
+
+	let nav_links = [
+		{
+			href: '/#intro',
+			text: 'Intro'
+		},
+		{
+			href: '/#sponsors',
+			text: 'Sponsors'
+		},
+		{
+			href: '/#speakers',
+			text: 'Speakers'
+		},
+		{
+			href: '/#faq',
+			text: 'FAQ'
+		}
+	];
+</script>
+
+<Head
+	title={$page.data.meta.title}
+	description={$page.data.meta.description}
+	url={$page.url.toString()}
+	image="https://sveltesummit.com/metaimg.png?2382234234623234843"
+/>
+
+<nav class="fixed inset-x-0 w-full bg-black/75 p-6">
+	<ul class="flex flex-wrap flex-row place-content-center place-items-center gap-5 md:gap-5 center">
+		{#each nav_links as { href, text }}
+			<li>
+				<Link {href}>{text}</Link>
+			</li>
+		{/each}
+	</ul>
+</nav>
+<main><slot /></main>
