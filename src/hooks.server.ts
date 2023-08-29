f63692e7fb5f2d105afb9e0c974878282456179a
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-08-29 23:44:30 +0200

    remove unused hook

--- src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-// export async function handle({ event, resolve }) {   
-//     event.setHeaders({
-//         'X-Frame-Options': 'SAMEORIGIN'
-//     })
-//     const response = await resolve(event);
-//     return response;
-//   }
