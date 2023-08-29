cf220b2e4769db1a01a1243d963a6d9fb27bf40b
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-08-29 23:52:53 +0200

    fix headers

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,7 @@
 export const load = (({ setHeaders }) => {
     setHeaders({
-        'X-Frame-Options': 'SAMEORIGIN'
+        'X-Frame-Options': 'SAMEORIGIN',
+        'X-Content-Type-Options': 'nosniff',
+        'Content-Security-Policy': 'frame-src youtube.com https://www.youtube.com'
     })
 })
