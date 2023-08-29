df980bafa6443c6c921b7b9fd9695dbc71b8d56c
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-08-29 23:46:27 +0200

    add CSP meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,13 @@
 	import { page } from '$app/stores';
 </script>
 
+<svelte:head>
+	<meta
+		http-equiv="Content-Security-Policy"
+		content="default-src 'self'; img-src https://*; child-src 'none'; frame-src youtube.com https://www.youtube.com;"
+	/>
+</svelte:head>
+
 <Head
 	title={$page.data.meta.title}
 	description={$page.data.meta.description}
