3499af5d3d001642730f01477dbd65794926f1c2
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-09-19 14:15:18 +0200

    fix sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -3,6 +3,12 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
   return await sitemap.response({
-    origin: 'https://sveltesummit.com'
+    origin: 'https://sveltesummit.com',
+    excludePatterns: [
+        '.*\\[slug\\].*',
+        '.*\\[year\\].*',
+        '/admin.*',
+        '/email-confirmed'
+    ]
   });
 };
