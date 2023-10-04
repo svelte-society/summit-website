d99b90c55a4a12e53d559a2925334c7ebf906be2
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-10-04 16:55:28 +0200

    fix height issues

--- src/routes/(app)/Sponsor.svelte
+++ src/routes/(app)/Sponsor.svelte
@@ -14,21 +14,29 @@
 	<li class="bg-blue-800 p-10 rounded-md text-white gap-3 hover:scale-[1.02] transition-transform">
 		<a href={sponsor.href} target="_blank" rel="noreferrer">
 			<picture>
-				<!-- load webp if supported -->
-				<source
-					srcset="//wsrv.nl/?url={PUBLIC_API_URL}/files/sponsors/{sponsor.id}/{sponsor.logo}&h={height *
-						2}&output=webp"
-					type="image/webp"
-				/>
-				<!-- load in case no `source` format applies 
-										  and use attributes for presentation -->
-				<img
-					height="{height}px"
-					class="mx-auto"
-					src="//wsrv.nl/?url={PUBLIC_API_URL}/files/sponsors/{sponsor.id}/{sponsor.logo}&h={height *
-						2}"
-					alt="{sponsor.name} logo"
-				/>
+				{#if !sponsor.logo.endsWith('.svg')}
+					<!-- load webp if supported -->
+					<source
+						srcset="//wsrv.nl/?url={PUBLIC_API_URL}/files/sponsors/{sponsor.id}/{sponsor.logo}&h={height *
+							2}&output=webp"
+						type="image/webp"
+					/>
+					<!-- load in case no `source` format applies 
+									  and use attributes for presentation -->
+					<img
+						height="{height}px max-h-28"
+						class="mx-auto"
+						src="//wsrv.nl/?url={PUBLIC_API_URL}/files/sponsors/{sponsor.id}/{sponsor.logo}&h={height *
+							2}"
+						alt="{sponsor.name} logo"
+					/>
+				{:else}
+					<img
+						class="mx-auto max-h-28"
+						src="{PUBLIC_API_URL}/files/sponsors/{sponsor.id}/{sponsor.logo}"
+						alt="{sponsor.name} logo"
+					/>
+				{/if}
 			</picture>
 		</a>
 	</li>
