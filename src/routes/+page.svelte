7889c115b5904096afd6e44ef3e257b056f0f93b
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-10-09 15:55:32 +0200

    Create Conference endpoint to reploce other endpoints

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,35 @@
-<div />
+<div class="grid landing-page-grid w-full h-">
+	<div class="bg-blue-400 current">Current</div>
+	<div class="bg-red-400 next">Next</div>
+	<div class="bg-yellow-400 past">Past</div>
+</div>
+
+<style>
+	:global(body) {
+		@apply bg-black text-white;
+	}
+	.landing-page-grid {
+		grid-template-rows: 1fr 1fr 150px;
+		grid-template-areas:
+			'current'
+			'next'
+			'past';
+		height: 100svh;
+
+		@media screen and (min-width: 680px) {
+			grid-template-rows: 1fr 150px;
+			grid-template-areas:
+				'current next'
+				'past past';
+		}
+	}
+	.current {
+		grid-area: current;
+	}
+	.past {
+		grid-area: past;
+	}
+	.next {
+		grid-area: next;
+	}
+</style>
---
+++ src/routes/api/conference/+server.ts
@@ -0,0 +1,28 @@
+import PocketBase from 'pocketbase';
+import { PUBLIC_API_URL } from '$env/static/public';
+import { POCKETBASE_PASSWORD, POCKETBASE_USERNAME } from '$env/static/private';
+import { fail, json } from '@sveltejs/kit'
+
+export const prerender = true
+
+export const GET = (async ({ url, fetch, request }) => {
+    const params = new URLSearchParams(url.search);
+    const year = params.get('year')
+    const season = params.get('season')
+
+    if (!year || !season) {
+        throw fail(404)
+    }
+
+    const pb = new PocketBase('https://summit-api-v2.sveltesociety.dev');
+    await pb.admins.authWithPassword(POCKETBASE_USERNAME, POCKETBASE_PASSWORD)
+
+    const conf = await pb.collection('Conference').getFirstListItem(`Year='${year}' && Season='${season}'`, {
+        expand: 'Sponsors,Talks,MC,Questions,Statistics,Highlights',
+    });
+
+    const expandedConf = {...conf, ...conf.expand}
+    delete expandedConf.expand
+
+    return json(expandedConf)
+  })
