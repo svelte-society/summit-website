fad0096cd4f0602aed13541f91c45b026f52eea5
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-10-09 22:28:32 +0200

    move load function to layout

--- src/routes/(app)/[year]/[season]/+layout.server.ts
+++ src/routes/(app)/[year]/[season]/+layout.server.ts
@@ -4,3 +4,40 @@ export const config: Config = {
   runtime: 'edge',
   regions: 'all'
 };
+
+export const load = (async ({ fetch, request }) => {
+  const res = await fetch('/api/conference?year=2023&season=fall')
+  const conference = await res.json()
+
+  const platinum = conference.sponsors.filter(sponsor => sponsor.type === 'platinum')
+  const gold = conference.sponsors.filter(sponsor => sponsor.type === 'gold')
+
+  const sponsors = {
+      platinum ,
+      gold
+  }
+
+  const meta = {
+      title: conference.meta_title,
+      description: conference.meta_description,
+      image: conference.meta_img,
+      collectionId: conference.collectionId,
+      recordId: conference.id
+  }
+
+  return { questions: conference.questions, sponsors, sessions: conference.talks, mcs: conference.mc, meta, subtitle: conference.title, date: formatDate(new Date(conference.date)) };
+})
+
+/**
+ * Converts a JS date object to a custom formatted string.
+ *
+ * @param {Date} date - The date object to format.
+ * @returns {string} - The formatted date string.
+ */
+function formatDate(date: Date): string {
+  const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];    const day = date.getUTCDate();
+  const monthIndex = date.getUTCMonth();
+  const year = date.getUTCFullYear();
+
+  return `${months[monthIndex]} ${day} ${year}`;
+}
--- src/routes/(app)/[year]/[season]/+page.server.ts
+++ src/routes/(app)/[year]/[season]/+page.server.ts
@@ -8,28 +8,9 @@ const emailSchema = z.object({
     email: z.string().email()
 })
 
-export const load = (async ({ fetch, request }) => {
-    const res = await fetch('/api/conference?year=2023&season=fall')
-    const conference = await res.json()
-
-    const platinum = conference.sponsors.filter(sponsor => sponsor.type === 'platinum')
-    const gold = conference.sponsors.filter(sponsor => sponsor.type === 'gold')
-
-    const sponsors = {
-        platinum ,
-        gold
-    }
-
-    const meta = {
-        title: conference.meta_title,
-        description: conference.meta_description,
-        image: conference.meta_img,
-        collectionId: conference.collectionId,
-        recordId: conference.id
-    }
-
+export const load = (async ({ request }) => {
     const form = await superValidate(request, emailSchema)
-    return { form, questions: conference.questions, sponsors, sessions: conference.talks, mcs: conference.mc, meta, subtitle: conference.title, date: formatDate(new Date(conference.date)) };
+    return { form };
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
@@ -126,17 +107,3 @@ async function getSHA1Hash(input: string) {
       .join("");
     return hash;
   };
-
-/**
- * Converts a JS date object to a custom formatted string.
- *
- * @param {Date} date - The date object to format.
- * @returns {string} - The formatted date string.
- */
-function formatDate(date: Date): string {
-    const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];    const day = date.getUTCDate();
-    const monthIndex = date.getUTCMonth();
-    const year = date.getUTCFullYear();
-  
-    return `${months[monthIndex]} ${day} ${year}`;
-}
