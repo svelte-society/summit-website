ec26031c113c841ac69181aae816cd63e2d43f2f
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-10-13 13:53:34 +0200

    adjust styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@
 				<h1
 					class="font-display gap-2 grid md:grid-cols-[3fr,2fr] place-content-center hover:scale-[1.01] transition-transform p-4"
 				>
-					<div class="text-center md:text-right lg:text-8xl text-7xl title">
+					<div class="text-center md:text-right lg:text-8xl text-7xl break-words">
 						SVELTE SUMMIT <span class="text-secondary">{season.toUpperCase()}</span>
 					</div>
 					<div
