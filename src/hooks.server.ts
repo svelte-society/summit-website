fcd2b0a85b1dd032f51c4e6361f462c899889bd8
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-10-13 13:28:14 +0200

    remvove api check

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,9 @@
 export async function handle({ event, resolve }) {
-    if (!event.url.pathname.startsWith('/api')) {
-        event.setHeaders({
-            'X-Frame-Options': 'SAMEORIGIN',
-            'X-Content-Type-Options': 'nosniff',
-            'Content-Security-Policy': 'frame-src youtube.com https://www.youtube.com'
-        })
-    }
+    event.setHeaders({
+        'X-Frame-Options': 'SAMEORIGIN',
+        'X-Content-Type-Options': 'nosniff',
+        'Content-Security-Policy': 'frame-src youtube.com https://www.youtube.com'
+    })
     
     return await resolve(event);
   }
