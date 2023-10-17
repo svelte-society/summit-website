3587cffbd34cc2c2dc11df25ae1cb918754ecef4
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-10-17 13:18:14 +0200

    start work on implementing og-image generation for talk pages

---
+++ src/lib/og-gen/renderImage.ts
@@ -0,0 +1,39 @@
+import satori from 'satori';
+import { Resvg } from '@resvg/resvg-js';
+import { html as toReactNode } from 'satori-html';
+
+import type { SvelteComponent } from 'svelte';
+
+// we use a Vite plugin to turn this import into the result of fs.readFileSync during build
+import sourceSerifPro from '$lib/fonts/SourceSerifPro-Regular.ttf';
+
+export async function componentToPng(component: SvelteComponent, props: any, height: number, width: number) {
+	const result = component.render(props);
+	const markup = toReactNode(`${result.html}<style>${result.css.code}</style>`);
+	const svg = await satori(markup, {
+		fonts: [
+			{
+				name: 'Source Serif Pro',
+				data: Buffer.from(sourceSerifPro),
+				style: 'normal'
+			}
+		],
+		height: +height,
+		width: +width
+	});
+
+	const resvg = new Resvg(svg, {
+		fitTo: {
+			mode: 'width',
+			value: +width
+		}
+	});
+
+	const png = resvg.render();
+
+	return new Response(png.asPng(), {
+		headers: {
+			'content-type': 'image/png'
+		}
+	});
+}
