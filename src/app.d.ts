7b7f9fc38e41a51258a22d58478c556196f1b9cb
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-10-26 16:05:12 +0200

    add pb types to locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { TypedPocketBase } from "$lib/pocketbase-types";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: TypedPocketBase
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
