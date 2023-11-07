9841a1e191fb32a2ae82500ed7deb103ff10981f
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-11-07 15:28:08 +0100

    minor work on redirect endpoint

--- src/routes/api/login/redirect/[service]/[...redirect]/+server.ts
+++ src/routes/api/login/redirect/[service]/[...redirect]/+server.ts
@@ -7,8 +7,6 @@ export const GET: RequestHandler = async ({ url, locals, params }) => {
 
     const { authProviders } = await pb.collection('users').listAuthMethods();
     const [provider] = authProviders.filter(provider => provider.name === service)
-    console.log(provider)
-    console.log(state)
     const authData = await pb.collection('users').authWithOAuth2Code(
         service,
         code,
@@ -16,15 +14,11 @@ export const GET: RequestHandler = async ({ url, locals, params }) => {
         url.origin + '/' + redirect
     )
 
-    console.log(authData)
+    const name = authData?.meta?.name ?? '';
+    const avatarUrl = authData?.meta?.avatar ?? '';
 
-    // const name = authData?.meta?.name ?? '';
-    // const avatarUrl = authData?.meta?.avatar ?? '';
-
-    // await pb.collection('users').update(authData.record.id, {
-    //     name,
-    //     avatarUrl
-    // });
-
-    return new Response('Hello!');
+    await pb.collection('users').update(authData.record.id, {
+        name,
+        avatarUrl
+    });
 };
