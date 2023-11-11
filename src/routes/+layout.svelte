ad0e2e9e3109ce60423c46bd1678dfa5c4f236cb
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-11-11 22:24:30 +0100

    update layout to use runes mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,15 @@
 	import { Head } from 'svead';
 	import { page } from '$app/stores';
 	import '../app.postcss';
+
+	const { data } = $props();
 </script>
 
 <Head
-	title={$page.data.meta.title}
-	description={$page.data.meta.description}
+	title={data.meta.title}
+	description={data.meta.description}
 	url={$page.url.toString()}
-	image="//wsrv.nl/?url={$page.data.meta.image}&output=png&maxage=7d"
+	image="//wsrv.nl/?url={data.meta.image}&output=png&maxage=7d"
 />
 
 <slot />
