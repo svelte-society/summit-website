400003cf1ff777d05de690a8f7c6f9d1681cff72
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2023-12-02 11:57:06 +0100

    expand types on satori endpoint

--- src/routes/api/satori/[id].png/+server.ts
+++ src/routes/api/satori/[id].png/+server.ts
@@ -1,7 +1,13 @@
 import { componentToPng } from './renderImage';
 import OG from './OG.svelte'
+import RawOG from './OG.svelte?raw'
 import PocketBase from 'pocketbase';
 import { PUBLIC_API_URL, } from '$env/static/public';
+import type { ConferenceResponse, SpeakerResponse, TalkResponse } from '$lib/pocketbase-types';
+
+type TExpand = {
+    talks: TalkResponse<SpeakerResponse>
+}
 
 export const prerender = true
 
@@ -16,7 +22,7 @@ export const entries = async () => {
 export const GET = async ({ params, locals }) => {
     const { pb } = locals
     const filter = pb.filter('talks ~ {:id}', { id: params.id })
-    const conference = await pb.collection('Conference').getFirstListItem(filter, {
+    const conference = await pb.collection<ConferenceResponse<TExpand>>('Conference').getFirstListItem(filter, {
         expand: 'talks,talks.speakers',
         fields: 'expand.talks.title,expand.talks.expand,expand.talks.id.speakers,primary_color,secondary_color,title,subtitle,year,season'
     });
