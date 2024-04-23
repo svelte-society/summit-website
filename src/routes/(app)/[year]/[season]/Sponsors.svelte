1e274466234f9f941cc96154608dd4ae34915a84
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-04-23 14:24:26 +0200

    wip: show speakers

--- src/routes/(app)/[year]/[season]/Sponsors.svelte
+++ src/routes/(app)/[year]/[season]/Sponsors.svelte
@@ -11,19 +11,15 @@
 	}>();
 </script>
 
-{#if is_open_to_sponsorships || (main && main.length > 1)}
+{#if is_open_to_sponsorships || (main?.length > 1)}
 	<article id="sponsors" class="bg-secondary text-black">
-		<div
-			class="grid gap-4 w-full sm:w-4/5 md:w-3/5 mx-auto px-4 md:py-16 py-8 text-center max-w-4xl"
-		>
+		<div class="grid gap-4 w-full sm:w-4/5 md:w-3/5 mx-auto px-4 md:py-16 py-8 text-center max-w-4xl">
 			<h2 class="font-semibold text-2xl">Main Partner</h2>
 			<hr class="border-black border-2 rounded-lg w-52 mx-auto" />
 			<div class="grid gap-3">
-				<ul class="grid grid-cols-1 gap-3">
-					{#each main as sponsor}
-						<Sponsor {sponsor} is_platinum={true}>👑 Main Partner</Sponsor>
-					{/each}
-				</ul>
+				{#each main as sponsor}
+					<Sponsor {sponsor} is_platinum={true}>👑 Main Partner</Sponsor>
+				{/each}
 				{#if is_open_to_sponsorships}
 					<a
 						class="bg-primary hover:brightness-110 transition-colors px-4 py-3 rounded-lg text-white font-semibold w-full sm:w-56 mx-auto"
