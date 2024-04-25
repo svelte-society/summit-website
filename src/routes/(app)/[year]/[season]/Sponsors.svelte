605db5dc8b9b565f1191c69f32ab489c7b476bc1
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-04-25 13:57:11 +0200

    add convex as partner

--- src/routes/(app)/[year]/[season]/Sponsors.svelte
+++ src/routes/(app)/[year]/[season]/Sponsors.svelte
@@ -2,25 +2,31 @@
 	import { page } from '$app/stores';
 	import type { SponsorResponse } from '$lib/pocketbase-types';
 	import Sponsor from './Sponsor.svelte';
+	import Button from '$lib/components/Button.svelte';
 
-	const { main, platinum, gold, is_open_to_sponsorships } = $props<{
-		main: (SponsorResponse | undefined)[];
+	const { partner, platinum, gold, open_to_sponsorships } = $props<{
+		partner: (SponsorResponse | undefined)[];
 		platinum: (SponsorResponse | undefined)[];
 		gold: (SponsorResponse | undefined)[];
-		is_open_to_sponsorships: boolean;
+		open_to_sponsorships: boolean;
 	}>();
 </script>
 
-{#if is_open_to_sponsorships || (main?.length > 1)}
+{#if open_to_sponsorships || (partner?.length > 0)}
 	<article id="sponsors" class="bg-secondary text-black">
 		<div class="grid gap-4 w-full sm:w-4/5 md:w-3/5 mx-auto px-4 md:py-16 py-8 text-center max-w-4xl">
 			<h2 class="font-semibold text-2xl">Main Partner</h2>
 			<hr class="border-black border-2 rounded-lg w-52 mx-auto" />
 			<div class="grid gap-3">
-				{#each main as sponsor}
+				{#each partner as sponsor}
 					<Sponsor {sponsor} is_platinum={true}>👑 Main Partner</Sponsor>
+					{#if sponsor.snippet}
+						<div class="bg-primary rounded-md py-6 px-8 text-left prose text-gray-100 prose-sm">
+							{@html sponsor.snippet}
+						</div>
+					{/if}
 				{/each}
-				{#if is_open_to_sponsorships}
+				{#if open_to_sponsorships}
 					<a
 						class="bg-primary hover:brightness-110 transition-colors px-4 py-3 rounded-lg text-white font-semibold w-full sm:w-56 mx-auto"
 						href="/{$page.params.year}/{$page.params.season}/sponsors">Become the main partner</a
@@ -47,7 +53,7 @@
 				{/each}
 			</ul>
 		</div>
-		{#if is_open_to_sponsorships}
+		{#if open_to_sponsorships}
 			<a
 				class="bg-primary hover:brightness-110 transition-colors px-4 py-3 rounded-lg text-white font-semibold w-full sm:w-56 mx-auto"
 				href="/{$page.params.year}/{$page.params.season}/sponsors">Become a sponsor</a
