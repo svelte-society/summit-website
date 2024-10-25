6813e08b74eccd08cefc6b2e923861f059205010
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-10-25 21:15:20 +0200

    Add initial hook setut

---
+++ src/hooks.server.ts
@@ -0,0 +1,5 @@
+// import { sequence } from "@sveltejs/kit/hooks";
+// import { add_user_data } from "./hooks/add_user_data";
+// import { protect_routes } from "./hooks/protect_routes";
+
+// export const handle = sequence(add_user_data, protect_routes);
