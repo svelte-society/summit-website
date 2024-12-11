d2e565549308e0c4fe7bd5b33ce2a2e5e92028ed
Karim Jordan <karimfromjordan@gmail.com>
2024-12-11 19:10:41 +0700

    add copyright

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,22 +1,19 @@
 <script lang="ts">
-		import Link from '$lib/components/Link.svelte';
+	import Link from '$lib/components/Link.svelte';
 </script>
 
 <section id="footer" class="text-center">
 	<div class="wrapper vertical-top container">
 		<div class="column">
-			<Link 
+			<Link
 				class="mb-1"
 				bg="var(--black)"
 				fg="var(--orange)"
-				href=""
-				target="_blank"><h4 class="no-caps">Svelte Summit AB</h4></Link
+				href="https://www.sveltesociety.dev/"
+				target="_blank"><h4 class="no-caps">Svelte Society</h4></Link
 			>
-			<Link
-				bg="var(--black)"
-				fg="var(--orange)"
-				href=""
-				target="_blank"><h4 class="no-caps">Privacy policy</h4></Link
+			<Link bg="var(--black)" fg="var(--orange)" href="" target="_blank"
+				><h4 class="no-caps">Privacy policy</h4></Link
 			>
 		</div>
 		<ul class="column">
@@ -25,50 +22,51 @@
 			</li>
 			<li>
 				<Link
-				bg="var(--black)"
-				fg="var(--orange)"
-				href="https://www.sveltesummit.com/2024/fall"
-				target="_blank"><h4 class="no-caps">Fall 2024</h4></Link
+					bg="var(--black)"
+					fg="var(--orange)"
+					href="https://www.sveltesummit.com/2024/fall"
+					target="_blank"><h4 class="no-caps">Fall 2024</h4></Link
 				>
 			</li>
 			<li>
 				<Link
-				bg="var(--black)"
-				fg="var(--orange)"
-				href="https://www.sveltesummit.com/2024/spring"
-				target="_blank"><h4 class="no-caps">Spring 2024</h4></Link
-			>
-			</li>
-			<li>
-				<Link
-				bg="var(--black)"
-				fg="var(--orange)"
-				href="https://www.sveltesummit.com/2023/fall"
-				target="_blank"><h4 class="no-caps">Fall 2023</h4></Link
+					bg="var(--black)"
+					fg="var(--orange)"
+					href="https://www.sveltesummit.com/2024/spring"
+					target="_blank"><h4 class="no-caps">Spring 2024</h4></Link
 				>
 			</li>
 			<li>
 				<Link
-				bg="var(--black)"
-				fg="var(--orange)"
-				href="https://www.sveltesummit.com/2023/spring"
-				target="_blank"><h4 class="no-caps">Spring 2023</h4></Link
+					bg="var(--black)"
+					fg="var(--orange)"
+					href="https://www.sveltesummit.com/2023/fall"
+					target="_blank"><h4 class="no-caps">Fall 2023</h4></Link
+				>
+			</li>
+			<li>
+				<Link
+					bg="var(--black)"
+					fg="var(--orange)"
+					href="https://www.sveltesummit.com/2023/spring"
+					target="_blank"><h4 class="no-caps">Spring 2023</h4></Link
 				>
 			</li>
 		</ul>
 	</div>
+	<div class="copyright">Copyright Svelte Summit AB</div>
 </section>
 
 <style>
 	#footer {
 		background: var(--black);
-		display:block;
-		padding:5rem 25rem;
+		display: block;
+		padding: 5rem 25rem;
 	}
 
-	@media (max-width:999px) {
+	@media (max-width: 999px) {
 		#footer {
-			padding:3rem;
+			padding: 3rem;
 		}
 	}
 
@@ -80,17 +78,22 @@
 	}
 
 	#footer h4 {
-		text-decoration:underline var(--orange);
+		text-decoration: underline var(--orange);
 	}
 
 	.text-decor-none {
-		text-decoration:none !important;
+		text-decoration: none !important;
 	}
 
 	#footer ul {
 		list-style-type: none;
-  		margin: 0;
-  		padding: 0;
+		margin: 0;
+		padding: 0;
 	}
-
+	.copyright {
+		font-weight: 600;
+		margin-block-start: 2rem;
+		font-size: 1rem;
+		color: white;
+	}
 </style>
